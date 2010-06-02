5f65d9cf5e11bafc01d828e851bd94d86d44efcf
Chad Payne <chad@bird-dog.ca>
2010-06-02 14:58:24 -0600

    Staff Email form changes

--- custom.js
+++ custom.js
@@ -498,7 +498,7 @@ jQuery(function($) {
         $('#emailWindow #staff-email').attr('value', email);
         $('body').append('<div id="jquery-overlay"></div>');
         $('#jquery-overlay').css({zIndex:200,background:'black',opacity:0.85,height:$(document).height()}).fadeIn();
-        $('#emailWindow').fadeIn();
+        $('#emailWindow').show().fadeIn();
         $('div#jquery-overlay').click(function(){
           $('body #jquery-overlay').remove();
           $('#emailWindow').hide();
